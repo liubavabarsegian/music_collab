f4cd3207ac9e3989fc59148ca7bb5ebb88a5f579
liubavabarsegian <lyubahemmo@mail.ru>
2023-09-20 21:12:50 +0300

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  resource :session, only: %i[new create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'login', to: 'sessions#new'
-  get 'new', to: 'users#new'
 end
